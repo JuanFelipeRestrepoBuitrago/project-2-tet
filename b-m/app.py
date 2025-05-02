9bf70e12573c53da038ddd85d7f7cbf559fef938
Kevin Quiroz <kquirozg@eafit.edu.co>
2025-05-02 12:19:48 -0500

    Refactoring codes

--- b-m/app.py
+++ b-m/app.py
@@ -2,19 +2,22 @@ from flask import Flask, render_template
 from extensions import db, login_manager
 from models.user import User
 
+# Inicializar la aplicación
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secretkey'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bookstore.db'
 
+# Inicializar extensiones
 db.init_app(app)
 login_manager.init_app(app)
 login_manager.login_view = 'auth.login'
 
+# Cargar usuario para Flask-Login
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-# Luego importar blueprints
+# Registrar Blueprints
 from controllers.auth_controller import auth
 from controllers.book_controller import book
 from controllers.purchase_controller import purchase
@@ -22,7 +25,6 @@ from controllers.payment_controller import payment
 from controllers.delivery_controller import delivery
 from controllers.admin_controller import admin
 
-# Registrar blueprints
 app.register_blueprint(auth)
 app.register_blueprint(book, url_prefix='/book')
 app.register_blueprint(purchase)
@@ -30,27 +32,27 @@ app.register_blueprint(payment)
 app.register_blueprint(delivery)
 app.register_blueprint(admin)
 
-from models.delivery import DeliveryProvider
-
-def initialize_delivery_providers():
-    with app.app_context():
-        if DeliveryProvider.query.count() == 0:
-            providers = [
-                DeliveryProvider(name="DHL", coverage_area="Internacional", cost=50.0),
-                DeliveryProvider(name="FedEx", coverage_area="Internacional", cost=45.0),
-                DeliveryProvider(name="Envia", coverage_area="Nacional", cost=20.0),
-                DeliveryProvider(name="Servientrega", coverage_area="Nacional", cost=15.0),
-            ]
-            db.session.bulk_save_objects(providers)
-            db.session.commit()
-
+# Ruta principal
 @app.route('/')
 def home():
     return render_template('home.html')
 
+# Inicializar proveedores de entrega si no existen
+from models.delivery import DeliveryProvider
+
+def initialize_delivery_providers():
+    if DeliveryProvider.query.count() == 0:
+        providers = [
+            DeliveryProvider(name="DHL", coverage_area="Internacional", cost=50.0),
+            DeliveryProvider(name="FedEx", coverage_area="Internacional", cost=45.0),
+            DeliveryProvider(name="Envia", coverage_area="Nacional", cost=20.0),
+            DeliveryProvider(name="Servientrega", coverage_area="Nacional", cost=15.0),
+        ]
+        db.session.bulk_save_objects(providers)
+        db.session.commit()
+
+# Ejecución principal
 if __name__ == '__main__':
-# OJO este conexto crea las tablas e inicia los proveedores de entrega, 
-# se debe ejecutar cada que se reinstala y ejecuta la aplicación Bookstore
     with app.app_context():
         db.create_all()
         initialize_delivery_providers()
--- b-m/extensions.py
+++ b-m/extensions.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
+# Extensiones globales para usar en toda la app
 db = SQLAlchemy()
 login_manager = LoginManager()
