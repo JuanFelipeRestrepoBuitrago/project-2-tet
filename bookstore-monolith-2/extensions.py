dd3ceafbec0b1841585e3dcb34911962c7f846a2
Julian Valencia <valenciajuliann@hotmail.com>
2025-05-03 17:26:32 -0500

    Cambios para desplegar en contenedor

--- bookstore-monolith-2/extensions.py
+++ bookstore-monolith-2/extensions.py
@@ -14,20 +14,15 @@ class RoutingSQLAlchemy(SQLAlchemy):
         """Inicializa la aplicación con soporte para enrutamiento de DB"""
         super().init_app(app)
         
-        # Obtener las URLs de las bases de datos
         self.write_engine_url = app.config['SQLALCHEMY_DATABASE_URI_WRITE']
         self.read_engine_url = app.config['SQLALCHEMY_DATABASE_URI_READ']
         
-        # Si no hay URL de lectura específica, usar la de escritura para ambas
         if not self.read_engine_url:
             self.read_engine_url = self.write_engine_url
             
-        # Crear una sesión con enrutamiento
         session = create_routing_session(self.write_engine_url, self.read_engine_url)
         
-        # Reemplazar la sesión de SQLAlchemy con nuestra sesión personalizada
         self.session = session
 
-# Extensiones globales para usar en toda la app
 db = RoutingSQLAlchemy()
 login_manager = LoginManager()
